f0fb0d33d152fa4459515b4f2db23ad362138533
Alexander Mehta <alexandermehta@outlook.com>
2022-03-07 19:23:35 -0800

    h2 server setup

---
+++ src/main/java/com/trie/server/TrieServerApplication.java
@@ -0,0 +1,13 @@
+package com.trie.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrieServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrieServerApplication.class, args);
+    }
+
+}
