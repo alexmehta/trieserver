08e6094dc9e9bd696993dbd92659c1415eed9bcf
Alexander Mehta <alexishanmehta@gmail.com>
2022-03-12 14:45:08 -0800

    update sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "pulling git repo"
-git pull
+git fetch origin master
+git reset --hard FETCH_HEAD
+git clean -df
 echo "packaging jar"
 mvn package -T4
 java -jar target/server-0.0.1-SNAPSHOT.jar
