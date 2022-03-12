ae89c075647b65c4deae782688af273ae204926b
Alex Mehta <alexishanmehta@gmail.com>
2022-03-11 23:58:25 -0800

    cli and server setup

--- src/cli/main.py
+++ src/cli/main.py
@@ -1,29 +1,35 @@
 import click
 import requests
-import os
 
-baseurl = "http://localhost:8821" + "/"
+
 @click.group()
 def main():
     pass
 
-@main.command()
-@click.argument('word')
-def insert(word):
-    response = requests.post(baseurl+"insert/" + word)
-    click.echo(response.content)
 
 @main.command()
 @click.argument('word')
-def find(word):
-    response = requests.get(baseurl+"find/" +word)
+@click.argument('baseurl')
+def insert(word, baseurl):
+    response = requests.post(baseurl + "/insert/" + word)
     click.echo(response.content)
+
 
 @main.command()
 @click.argument('word')
-def predict(word):
-    response = requests.get(baseurl+"predict/" + word)
+@click.argument('baseurl')
+def find(word, baseurl):
+    response = requests.get(baseurl + "/find/" + word)
     click.echo(response.content)
+
+
+@main.command()
+@click.argument('word')
+@click.argument('baseurl')
+def predict(word, baseurl):
+    response = requests.get(baseurl + "/predict/" + word)
+    click.echo(response.content)
+
 
 if __name__ == "__main__":
     main()
